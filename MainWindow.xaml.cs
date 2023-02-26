a9e5e513532d2a02d759f88a5f6a0b8765b5fa68
Thomaz Moura <padilha.thomaz@gmail.com>
2023-02-26 19:16:19 -0300

    Silly change to test event hooks - show the title on the overlay

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,42 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace SpotlightDimmer
 {
-    public partial class TransparentOverlayWindow : Window
+    public partial class MainWindow : Window
     {
+        private static int count = 0;
+
         private const int WS_EX_TRANSPARENT = 0x00000020;
         private const int GWL_EXSTYLE = (-20);
+        private const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
 
+        private IntPtr _hook;
+        private WinEventDelegate _winEventDelegate;
+
+        // Methods to make the window transparent to clicks
         [DllImport("user32.dll")]
         static extern int GetWindowLong(IntPtr hwnd, int index);
-
         [DllImport("user32.dll")]
         static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 
-        public TransparentOverlayWindow()
+        // Methods to get focus events
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+        
+        public MainWindow()
         {
             InitializeComponent();
 
@@ -27,6 +46,11 @@ namespace SpotlightDimmer
             Top = 0;
             Width = SystemParameters.PrimaryScreenWidth;
             Height = SystemParameters.PrimaryScreenHeight;
+            
+            _winEventDelegate = new WinEventDelegate(WinEventProc);
+            _hook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, _winEventDelegate, 0, 0, 0);
+
+            Info.Text = "Starting";
 
             // Show the window
             Show();
@@ -44,5 +68,35 @@ namespace SpotlightDimmer
             var hwnd = new WindowInteropHelper(this).Handle;
             SetWindowExTransparent(hwnd);
         }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            UnhookWinEvent(_hook);
+        }
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            const int nChars = 256;
+            StringBuilder sb = new StringBuilder(nChars);
+
+            var titulo = "N/A";
+            // get the title of the window
+            if (GetWindowText(hwnd, sb, nChars) > 0)
+            {
+                titulo = sb.ToString();
+            }
+
+            Info.Text = @$"Window focus event received {count++} times.
+Details:
+                
+Window Title={titulo}
+IdObject={idObject}
+IdChild={idChild}
+winEventHook={hWinEventHook}
+hwnd={hwnd}";
+        }
+
     }
 }
